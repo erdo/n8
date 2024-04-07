ee8eb14e913d4581d9d142d22d591f93a6bc6be6
erdo <226319+erdo@users.noreply.github.com>
2024-04-07 12:44:43 +0200

    docs update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,22 @@
+/**
+ * ./gradlew check
+ *
+ * ./gradlew test
+ * ./gradlew testDebugUnitTest
+ * ./gradlew connectedAndroidTest
+ *
+ * ./gradlew clean
+ * ./gradlew publishToMavenLocal
+ * ./gradlew publishReleasePublicationToMavenCentralRepository --no-daemon --no-parallel
+ *
+ * ./gradlew :buildEnvironment
+ *
+ * ./gradlew :persista-lib:dependencies
+ *
+ * git tag -a v1.5.9 -m 'v1.5.9'
+ * git push origin --tags
+ */
+
 buildscript {
     repositories {
         mavenCentral()
--- publish-lib.gradle.kts
+++ publish-lib.gradle.kts
@@ -1,21 +1,3 @@
-/**
- * ./gradlew check
- *
- * ./gradlew test
- * ./gradlew testDebugUnitTest
- * ./gradlew connectedAndroidTest
- *
- * ./gradlew clean
- * ./gradlew publishToMavenLocal
- * ./gradlew publishReleasePublicationToMavenCentralRepository --no-daemon --no-parallel
- *
- * ./gradlew :buildEnvironment
- *
- * ./gradlew :persista-lib:dependencies
- *
- * git tag -a v1.5.9 -m 'v1.5.9'
- * git push origin --tags
- */
 
 import co.early.n8.Shared
 import org.gradle.kotlin.dsl.`maven-publish`
