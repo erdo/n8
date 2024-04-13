0a6d37c398b7ba2bede9fd4021bfd06c683abc4b
erdo <226319+erdo@users.noreply.github.com>
2024-04-13 14:01:50 +0100

    show both state and UI in landscape mode

--- example-app/src/main/kotlin/foo/bar/n8/ui/common/StateViewer.kt
+++ example-app/src/main/kotlin/foo/bar/n8/ui/common/StateViewer.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
@@ -25,8 +27,9 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.early.fore.kt.core.delegate.Fore
+import co.early.fore.ui.size.AspectBasedComposable
 import co.early.fore.ui.size.LocalWindowSize
-import co.early.fore.ui.size.WidthBasedTextUnit
+import co.early.fore.ui.size.MinDimBasedTextUnit
 import co.early.fore.ui.size.WindowSize
 import foo.bar.n8.App
 import foo.bar.n8.R
@@ -45,47 +48,75 @@ fun StateWrapperView(
 
     val showStateFlag by stateFlagModel.toState()
 
-    AnimatedVisibility(
-        visible = !showStateFlag.showState,
-        enter = slideInVertically(initialOffsetY = { it / 2 }) + fadeIn(),
-        exit = slideOutVertically(targetOffsetY = { it / 2 }) + fadeOut(),
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        content()
-    }
+        AspectBasedComposable(
+            port = {
+                AnimatedVisibility(
+                    modifier = Modifier.align(Alignment.Center),
+                    visible = !showStateFlag.showState,
+                    enter = slideInVertically(initialOffsetY = { it / 2 }) + fadeIn(),
+                    exit = slideOutVertically(targetOffsetY = { it / 2 }) + fadeOut(),
+                ) {
+                    content()
+                }
+                AnimatedVisibility(
+                    modifier = Modifier.align(Alignment.Center),
+                    visible = showStateFlag.showState,
+                    enter = fadeIn(),
+                    exit = fadeOut(),
+                ) {
+                    StateView(stateAsString)
+                }
 
-    val stateFontSize = WidthBasedTextUnit(
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    DisplayToggleView(
+                        hideButtonVisible = showStateFlag.showState,
+                        size = size,
+                        toggleDisplayCallback = { stateFlagModel.toggle() },
+                    )
+                }
+            },
+            land = {
+                Row {
+                    Box(
+                        modifier = Modifier
+                            .width(size.dpSize.width / 3),
+                        contentAlignment = Alignment.TopCenter
+                    ) {
+                        content()
+                    }
+                    StateView(stateAsString)
+                }
+            }
+        )(size)
+    }
+}
+
+@Composable
+fun StateView(
+    stateAsString: String,
+    size: WindowSize = LocalWindowSize.current,
+) {
+    val stateFontSize = MinDimBasedTextUnit(
         xs = 12.sp,
         m = 20.sp,
         l = 35.sp
     )
-
-    AnimatedVisibility(
-        visible = showStateFlag.showState,
-        enter = fadeIn(),
-        exit = fadeOut(),
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = stateAsString,
-                style = TextStyle(fontSize = stateFontSize(size))
-            )
-        }
-    }
-
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start,
     ) {
-        DisplayToggleView(
-            hideButtonVisible = showStateFlag.showState,
-            size = size,
-            toggleDisplayCallback = { stateFlagModel.toggle() },
+        Text(
+            text = stateAsString,
+            style = TextStyle(fontSize = stateFontSize(size))
         )
     }
 }
@@ -99,7 +130,8 @@ fun BoxScope.DisplayToggleView(
 
     Fore.i("DisplayToggleView")
 
-    val label = stringResource(id = if (hideButtonVisible) R.string.hide_state else R.string.show_state)
+    val label =
+        stringResource(id = if (hideButtonVisible) R.string.hide_state else R.string.show_state)
     val alignment = if (size.isRound) Alignment.TopCenter else Alignment.TopEnd
 
     OutlinedButton(
